4218a01d0dcd7015cbf7ad4f54e5fc52d29772a5
Harounjlassi <jlassi.haroun@esprit.tn>
2025-04-20 10:51:13 +0100

    add: template

---
+++ Exam.Infrastructure/ExamContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Infrastructure
+{
+    public class ExamContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+
+             Initial Catalog=DB;Integrated Security=true;
+                  MultipleActiveResultSets=true");
+            optionsBuilder.UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+    }
+}
